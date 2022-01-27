11430879ed551a216d6a6da9fd407f12a2d1b9d6
64J0 <vinigaio97@gmail.com>
2022-01-27 08:53:16 -0300

    feat: update gitignore and add two other scripts to deal with cameras and videos

--- samples/capture-video.py
+++ samples/capture-video.py
@@ -1,14 +1,17 @@
 import cv2
 
-videoCapture = cv2.VideoCapture('MyInputVid.avi')
-fps = videoCapture.get(cv2.CAP_PROP_FPS)
-size = (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+cameraDeviceIndex = 0
+cameraCapture = cv2.VideoCapture(cameraDeviceIndex)
+fps = 30 # An assumption
+size = (int(cameraCapture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cameraCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 videoWriter = cv2.VideoWriter(
     'MyOutputVid.avi', cv2.VideoWriter_fourcc('I', '4', '2', '0'),
     fps, size)
 
-success, frame = videoCapture.read()
-while success: # Loop until there are no more frames
+success, frame = cameraCapture.read()
+numFramesRemaining = 10 * fps - 1 # 10 seconds of frames
+while success and numFramesRemaining > 0:
     videoWriter.write(frame)
-    success, frame = videoCapture.read()
+    success, frame = cameraCapture.read()
+    numFramesRemaining -= 1
